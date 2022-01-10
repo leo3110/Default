d43ba2e685d32488d0a05d756eeede76c9fd75d5
Leonardo NS <57919369+leo3110@users.noreply.github.com>
2022-01-10 15:42:17 -0300

    Update function.php

--- Script/function.php
+++ Script/function.php
@@ -3,3 +3,13 @@ elseif (isset($_GET['tela'])&&checkTela($_GET['tela'])) {
   $_SESSION['op']=$_GET['tela'];
   include 'GUI/'.$_GET['tela'].'.php';
 }
+//limita telas acessíveis
+define('Telas', ['home','tela1','tela2']);
+function checkTela($a){
+	foreach (tela as $key) {
+		if ($key == $a) {
+			return $a;
+		}
+		return "home";
+	}
+}
